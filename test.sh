557ec3c4f1d82c4cdbfee0c7820b80d17879e192
vishal rahikar <vishal.babasahebrahikar@ltts.com>
2019-03-05 17:56:35 +0530

    done some changes

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 echo "Welcome to Jenkins CI/CD pipeline"
 echo "Today's date is 1st march 2019"
-
+echo "Hey foxs done some changes pull it man now"
+#read name
+#echo $name
+echo "Today is tuesday"
